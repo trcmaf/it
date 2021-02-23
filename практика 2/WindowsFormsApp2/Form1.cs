4b762bdc2803d83ff548c32672f2dfa9e6d3154d
trcmaf <anna.v.sokolova20@gmail.com>
2021-02-23 13:36:46 +0300

    init

--- практика 2/WindowsFormsApp2/Form1.cs
+++ практика 2/WindowsFormsApp2/Form1.cs
@@ -19,6 +19,35 @@ namespace WindowsFormsApp2
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            textBox1.Text = "2";
+            // Вывод строки в многострочный редактор
+            textBox2.Text = "Практическая работа №2 Соколова А.Р.";
+            textBox2.Text += Environment.NewLine + "Рассчитать значение выражения y = x ^ 2";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Считывание значения X
+            double x = double.Parse(textBox1.Text);
+            // Вывод значения X в окно
+            textBox2.Text += Environment.NewLine +
+            "При x = " + x.ToString();
+            // Вычисляем арифметическое выражение
+            double y = Math.Pow(x, 2);
+
+            // Выводим результат в окно
+            textBox2.Text += Environment.NewLine +
+            "Результат y = " + y.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
 
         }
     }
