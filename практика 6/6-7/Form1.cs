6c4025402efb26b720be8c67d97f7a754a267d55
trcmaf <anna.v.sokolova20@gmail.com>
2021-03-23 14:39:36 +0300

    init

---
+++ практика 6/6-7/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6_7
+{
+    public partial class Form1 : Form
+    {
+        int[] Mas = new int[15];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            richTextBox1.Text = "";
+            for (int i = 0; i < 15; i++)
+            {
+                Mas[i] = rand.Next(-50, 50);
+                richTextBox1.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(Mas[i]) + Environment.NewLine;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            for (int i = 0; i < 15; i++)
+            {
+                if (Mas[i] < 0) Mas[i] = 0;
+                richTextBox2.Text += "Mas[" + Convert.ToString(i) + "] = " + Convert.ToString(Mas[i]) + Environment.NewLine;
+            }
+        }
+    }
+}
